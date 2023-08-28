835295e12d19f5cd99ffe0ca4e402f13c1ca688f
amit choudhary <amitchoudhary5759@gmail.com>
2023-08-28 18:07:40 +0530

    first commit

---
+++ src/Fact.java
@@ -0,0 +1,34 @@
+import com.sun.corba.se.spi.orbutil.fsm.StateImpl;
+
+import java.util.Scanner;
+
+
+class Rec {
+    static int Recursion(int n) {
+        if (n == 1) {
+            return 1;
+
+        }
+
+            return n * Recursion(n - 1);
+        }
+
+    }
+
+
+    public class Fact {
+        public static void main(String[] args) {
+            //int fact=Integer.MAX_VALUE;
+//            int fact = 1;
+//            Scanner sc = new Scanner(System.in);
+//            int n = sc.nextInt();
+//            for (int i = 1; i <= n; i++) {
+//                fact *= i;
+//            }
+//            System.out.println(fact);
+
+            System.out.println(Rec.Recursion(5));
+
+        }
+    }
+
